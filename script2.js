9b7fa67c57bcb7d113ab2702f1014564829704ff
pjrbors <peejayrb@gmail.com>
2026-03-19 17:47:23 +0800

    Enhance gift opening messages and titles

--- script2.js
+++ script2.js
@@ -1,7 +1,9 @@
+// Open the surprise popup
 function openSurprise() {
     document.getElementById("surprisePopup").classList.add("show");
 }
 
+// Close the surprise popup
 function closeSurprise() {
     const popup = document.getElementById("surprisePopup");
 
@@ -13,18 +15,23 @@ function closeSurprise() {
     }, 300);
 }
 
+// Open a gift based on choice
 function openGift(choice) {
     const result = document.getElementById("giftResult");
 
     if (choice === 1) {
+        // First gift: flower with title + message
         result.innerHTML = `
             <img src="https://raw.githubusercontent.com/pjrbors/for-my-wife/main/1773910831618.png" class="gift-img">
-            <p>For you 🌸</p>
+            <p class="gift-title">Flowers for you</p>
+            <p class="gift-message">You brighten my day every day 🌸</p>
         `;
     } else if (choice === 2) {
+        // Second gift: silly photo
         result.innerHTML = `
-            <img src="https://raw.githubusercontent.com/pjrbors/for-my-wife/main/IMG_20260319_172645.jpg" class="gift-img">
-            <p>Hehe got you 😝</p>
+            <img src="https://raw.githubusercontent.com/pjrbors/for-my-wife/main/photo.png" class="gift-img">
+            <p class="gift-title">Hehe got you</p>
+            <p class="gift-message">A little silly surprise 😝</p>
         `;
     }
-}
+    }
